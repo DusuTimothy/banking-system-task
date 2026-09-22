2923678077f50c4d65a007f4cb4af3e2f2aab4f3
Dusu Timothy <dususunday@gmail.com>
2026-09-22 14:58:33 +0100

    fix amount to balance from 0 to 2000

--- src/data/createUser.js
+++ src/data/createUser.js
@@ -9,7 +9,7 @@ function createUser({ fullName, email, passwordHash }) {
     email: email.toLowerCase(),
     passwordHash,
     pinHash: null, // set later via "create pin"
-    balance: 0,
+    balance: 2000,
     accountNumber: generateAccountNumber(),
     createdAt: new Date().toISOString(),
     updatedAt: new Date().toISOString(),
